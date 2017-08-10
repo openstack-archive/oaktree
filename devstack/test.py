429abd75362a580a0035607e73e710bbd7968b6b
Monty Taylor <mordred@inaugust.com>
2017-08-10 08:23:07 -0500

    Use new make_client method
    Change-Id: I03b69877d6f2076a4d8dcd044f9cc09573ec6656
    Depends-On: I4c27977802066a9f26f74df0d8386e9c4f85739e

--- devstack/test.py
+++ devstack/test.py
@@ -1,13 +1,24 @@
-# flake8: noqa
+# Copyright 2017 Red Hat, Inc.
+# Copyright 2016 IBM Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import sys
 
-import grpc
-
 from oaktreemodel import model
-from oaktreemodel import oaktree_pb2
 
-channel = grpc.insecure_channel('localhost:50051')
-stub = oaktree_pb2.OaktreeStub(channel)
+client = model.make_client('localhost:50051')
 
 cloud = model.Location()
 if len(sys.argv) > 1:
@@ -15,7 +26,7 @@ if len(sys.argv) > 1:
 else:
     cloud.cloud = 'devstack'
 
-flavors = stub.ListFlavors(model.Filter(location=cloud))
-print flavors
-images = stub.ListImages(model.Filter(location=cloud))
-print images
+flavors = client.ListFlavors(model.Filter(location=cloud))
+print(flavors)
+images = client.ListImages(model.Filter(location=cloud))
+print(images)
